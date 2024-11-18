550c83886e964f71aa5bcbf151c6fe71c4a30871
VictoriaCema <victoriacerettimadrid@gmail.com>
2024-11-18 03:01:46 +0000

    Punto D

---
+++ RTA_Examen_20241117/Punto_D.sh
@@ -0,0 +1,130 @@
+  358  cd UTN-FRA_SO_Examenes/
+  359  ls -l
+  360  cd 202406
+  361  ls -l
+  362  cd ansible/
+  363  ls -l
+  364  vim playbook.yml 
+  365  vim crear_estructura_directorios.yml
+  366  ansible-playbook crear_estructura_directorios.yml
+  367  sudo ansible-playbook crear_estructura_directorios.yml
+  368  sudo ansible-playbook playbook.yml
+  369  ls -l
+  370  cat ansible.cfg 
+  371  cd inventory/
+  372  ls -l
+  373  cd group_vars/
+  374  ls -l
+  375  cd
+  376  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/cd 202406
+  377  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/202406/ansible
+  378  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/
+  379  ls -l
+  380  UTN-f
+  381  cd UTN-FRA_SO_Examenes/
+  382  ls -l
+  383  cd 202406
+  384  cd ansible/
+  385  ls -l
+  386  cd /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible
+  387  cd alumno
+  388  find / -type d -name "alumno" 2>/dev/null
+  389  cd alumno
+  390  cd /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/alumno
+  391  ls -l
+  392  sudo chmod -R 755 /UTNFRA_SO_2do_Parcial_CerettiMadrid
+  393  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/
+  394  cd UTN-FRA_SO_Examenes/cd 202406
+  395  cd UTN-FRA_SO_Examenes/
+  396  cd 202406
+  397  cd ansible
+  398  ls -l
+  399  cd alumno
+  400  cd /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/alumno
+  422  sudo chmod -R 755 /UTNFRA_SO_2do_Parcial_CerettiMadrid
+  423  sudo chown -R $(whoami) /UTNFRA_SO_2do_Parcial_CerettiMadrid
+  424  ls -l /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  437  cd /path/to/repo/202406/ansible/
+  438  vim templates/datos_alumno.j2
+  439  cd /path/to/repo/202406/ansible/
+  440  nano templates/datos_alumno.j2
+  441  mkdir -p /path/to/repo/202406/ansible/templates
+  442  mkdir -p /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates
+  443  nano /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/datos_alumno.j2
+  444  sudo ansible-playbook crear_estructura_directorios.yml
+  445  cd /tmp/2do_parcial/alumno
+  446  cd templates
+  447  ls -l
+  448  vim crear_estructura_directorios.yml 
+  449  vim playbook.yml 
+  450  sudo ansible-playbook crear_estructura_directorios.yml
+  457  sudo chmod -R 777 /tmp
+  458  sudo mkdir -p /tmp/2do_parcial/alumno
+  466  ls -l /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  467  find / -type f -name "crear_estructura_directorios.yml" 2>/dev/null
+  493  sudo ansible-playbook crear_estructura_directorios.yml
+  494  cd
+  495  cd /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  496  cd alumno
+  497  ls -l
+  498  cd
+  499  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible
+  500  vim playbook.yml 
+  501  sudo ansible-playbook crear_estructura_directorios.yml
+  502  cd /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  503  cd alumno
+  504  ls -l
+  505  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible
+  506  vim p
+  507  cd
+  508  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible
+  509  vim p
+  510  vim playbook.yml 
+  511  nano /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/crear_estructura_directorios.yml
+  512  cd /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  513  sudo ansible-playbook crear_estructura_directorios.yml
+  514  cd /tmp/2do_parcial/alumno
+  515  ls -l
+  516  cd
+  517  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible
+  518  ls -l
+  519  vim crear_estructura_directorios.yml
+  520  sudo ansible-playbook crear_estructura_directorios.yml
+  521  vim crear_estructura_directorios.yml
+  522  vim crear_estructura_directorios.yml 
+  523  cd /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  524  mkdir -p /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  525  mv /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/datos_alumno.j2 /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  526  mkdir -p /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  527  nano /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/datos_alumno.j2
+  528  ls -l /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  529  cd /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  530  sudo ansible-playbook crear_estructura_directorios.yml
+  531  ls -l
+  532  cd /UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/
+  533  cd alumno
+  534  ls -l
+  535  cd /tmp/2do_parcial/alumno/
+  536  ls -l
+  537  cat datos_alumno.txt 
+  538  cd
+  539  cd UTNFRA_SO_2do_Parcial_CerettiMadrid/
+  540  cd /home/vagrant/UTNFRA_SO_2do_Parcial_CerettiMadrid/UTN-FRA_SO_Examenes/202406/ansible/templates/
+  541  nano datos_equipo.j2
+  542  cd ..
+  543  ls -l
+  544  vim crear_estructura_directorios.yml 
+  545  sudo ansible-playbook crear_estructura_directorios.yml
+  546  hostname -I
+  547  lsb_release -a
+  548  nproc
+  549  vim crear_estructura_directorios.yml 
+  550  sudo visudo
+  551  sudo groupadd 2PSupervisores
+  552  sudo usermod -aG 2PSupervisores vagrant
+  553  cd ..
+  554  git status
+  555  git add .
+  556  cd RTA_Examen_20241117/
+  557  ls -l
+  558  history >> Punto_D.sh
